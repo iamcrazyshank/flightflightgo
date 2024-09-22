8331d9f7922657e04fcd857de0e63d17bf0d357c
Shashank Chandran <iamcrazyshank@gmail.com>
2024-09-22 11:20:27 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/flight2go-logo.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Flight Flight Go
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
